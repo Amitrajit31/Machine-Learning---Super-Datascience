b264733e8b04474d22f90e502b63ddfcbee86492
karthik <iamkt17@gmail.com>
2018-03-28 13:08:27 +0530

    16 - Naive Bayes
    Classfier code added.

--- Part 3 - Classification/Section 18 - Naive Bayes/MyCode/naive_bayes.py
+++ Part 3 - Classification/Section 18 - Naive Bayes/MyCode/naive_bayes.py
@@ -21,7 +21,9 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Fitting Naive Bayes to the Training set
-
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)

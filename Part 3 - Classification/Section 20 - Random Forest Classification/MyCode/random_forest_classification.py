22acd6d62d53047b4b1096647b786432ee68a79c
karthik <iamkt17@gmail.com>
2018-03-28 14:49:50 +0530

    18 - Random Forest Classification
    Chapter completed

--- Part 3 - Classification/Section 20 - Random Forest Classification/MyCode/random_forest_classification.py
+++ Part 3 - Classification/Section 20 - Random Forest Classification/MyCode/random_forest_classification.py
@@ -21,6 +21,9 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Fitting Random Forest Classification to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)

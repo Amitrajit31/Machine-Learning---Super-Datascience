2fc6909baec0a20badb1c1808519825796d0b1ee
karthik <iamkt17@gmail.com>
2018-05-03 10:04:13 +0530

    22 - Hierarchical Clustering
    Chapter completed

--- Part 4 - Clustering/Section 25 - Hierarchical Clustering/MyCode/hc.py
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/MyCode/hc.py
@@ -7,16 +7,30 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
-X = dataset.iloc[:, [3, 4]].values
+x = dataset.iloc[:, [3, 4]].values
+
+# Using dendogram to find optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendogram = sch.dendrogram(sch.linkage(x, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distance')
+plt.show()
+
+# Fitting hierarchical clustering into mall data set
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters=5, affinity="euclidean", linkage='ward')
+y_hc = hc.fit_predict(x)
 
 # Visualising the clusters
-plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
-plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
-plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
-plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
-plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
-plt.title('Clusters of customers')
-plt.xlabel('Annual Income (k$)')
-plt.ylabel('Spending Score (1-100)')
+# Use only for the 2 dimension. Don't use for multidimension
+plt.scatter(x[y_hc == 0, 0], x[y_hc == 0, 1], s = 100, c = 'red', label = 'Careful')
+plt.scatter(x[y_hc == 1, 0], x[y_hc == 1, 1], s = 100, c = 'blue', label = 'Standard')
+plt.scatter(x[y_hc == 2, 0], x[y_hc == 2, 1], s = 100, c = 'green', label = 'Target')
+plt.scatter(x[y_hc == 3, 0], x[y_hc == 3, 1], s = 100, c = 'magenta', label = 'Careless')
+plt.scatter(x[y_hc == 4, 0], x[y_hc == 4, 1], s = 100, c = 'gray', label = 'Sensible')
+plt.title('Clusters of clients')
+plt.xlabel('Annual income (k$)')
+plt.ylabel('Sending score (1-100)')
 plt.legend()
 plt.show()

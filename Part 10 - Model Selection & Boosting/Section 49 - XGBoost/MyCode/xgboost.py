d0e1fbe869297890f0b92d83b2b678dedfc7b104
karthik <iamkt17@gmail.com>
2018-06-02 08:13:46 +0530

    39 - XGBoost
    Chapter completed

--- Part 10 - Model Selection & Boosting/Section 49 - XGBoost/MyCode/xgboost.py
+++ Part 10 - Model Selection & Boosting/Section 49 - XGBoost/MyCode/xgboost.py
@@ -27,7 +27,9 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Fitting XGBoost to the Training set
-
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
+classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
